eabd0767214e45b57041ee482e3af0254c4cb923
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-12-02 10:44:35 -0500

    Ajout du script dommage et modification du script vie

--- Assets/Scripts/Vie.cs
+++ Assets/Scripts/Vie.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Vie : MonoBehaviour{
     public float vieMax;
-    private float vieActuel{ get; set; }
+    private float vieActuel;
+    public bool doitAttendre{ set; private get; }
+    private float tempsAttente;
 
     public RectTransform foreground;
 
     private void Start(){
         vieActuel = vieMax;
-        perdreVie(1);
     }
 
-    public void perdreVie(int dommage){
-        vieActuel -= dommage;
-        foreground.sizeDelta = new Vector2(vieActuel / vieMax * 300, foreground.sizeDelta.y);
+    public void perdreVie(int dommage, float tempsAttente){
+        if (doitAttendre){
+            this.tempsAttente += Time.deltaTime;
+            if (this.tempsAttente >= tempsAttente){
+                vieActuel -= dommage;
+                foreground.sizeDelta = new Vector2(vieActuel / vieMax * 300, foreground.sizeDelta.y);
+                this.tempsAttente = 0;
+            }
+        } else{
+            vieActuel -= dommage;
+            foreground.sizeDelta = new Vector2(vieActuel / vieMax * 300, foreground.sizeDelta.y);
+            doitAttendre = true;
+        }
     }
 }

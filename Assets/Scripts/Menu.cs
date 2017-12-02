e9e1ab1d6826c7b292df7f3a67356d21ad595702
Frelin Maxime <frelinmaxime@gmail.com>
2017-12-02 22:52:08 +0100

    Ajout d'un script pour Jouer et Quitter

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public Button bouttonJouer;
+    public Button boutonQuitter;
+
+	public void quitter()
+    {
+        Application.Quit();
+    }
+
+    public void jouer()
+    {
+        bouttonJouer.gameObject.SetActive(false);
+        boutonQuitter.gameObject.SetActive(false);
+    }
+}

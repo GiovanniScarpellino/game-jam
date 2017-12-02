6c56b6bc163776f60f1b96a8380a6354b9c5a44c
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-12-02 09:28:39 -0500

    Création du script vie

---
+++ Assets/Scripts/Vie.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vie : MonoBehaviour{
+
+	public int vieMax;
+	private int vieActuel{ get; set; }
+	
+}

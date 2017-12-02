08c73c515d2ff998c518b4c2412ddc6da0039eb4
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-12-02 09:59:36 -0500

    Ajout du script vie

--- Assets/Scripts/Vie.cs
+++ Assets/Scripts/Vie.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Vie : MonoBehaviour{
+    public float vieMax;
+    private float vieActuel{ get; set; }
 
-	public int vieMax;
-	private int vieActuel{ get; set; }
-	
+    public RectTransform foreground;
+
+    private void Start(){
+        vieActuel = vieMax;
+        perdreVie(1);
+    }
+
+    public void perdreVie(int dommage){
+        vieActuel -= dommage;
+        foreground.sizeDelta = new Vector2(vieActuel / vieMax * 300, foreground.sizeDelta.y);
+    }
 }

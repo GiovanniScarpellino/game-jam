2747c77650d7fcee361790511b4a728a8d575223
Frelin Maxime <frelinmaxime@gmail.com>
2017-12-03 05:29:37 +0100

    Ajout du possible cassage d'arbre

---
+++ Assets/Scripts/CouperLesArbres.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CouperLesArbres : MonoBehaviour {
+    public GameObject arbre;
+    public MapGenerator mapGenerator;
+
+    private Vector3 vecteurDistance;
+    private float distance;
+
+	// Use this for initialization
+	void Start () {
+        mapGenerator = GameObject.FindGameObjectWithTag("MapGenerator").GetComponent<MapGenerator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouse.x = Mathf.Floor(mouse.x + 0.5f);
+            mouse.y = Mathf.Floor(mouse.y + 0.5f);
+            print(mapGenerator.arbreSurPosition(mouse));
+            if (mapGenerator.arbreSurPosition(mouse))
+            {
+                print("J'ai touché");
+                distance = 0f;
+                vecteurDistance = this.gameObject.transform.position - new Vector3(mouse.x, mouse.y, 0);
+                distance = vecteurDistance.magnitude;
+                distance = Mathf.Floor(distance - 0.5f);
+                print("distance : " + distance);
+                if (distance <= 1)
+                {
+                    print(distance);
+                }
+            }
+        }
+    }
+}

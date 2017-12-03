c697198b0ba5bda77a585894074a2ceb1a0a646a
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-12-03 10:58:20 -0500

    Mouvement du player avec le path finding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,5 +5,19 @@ using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
-    
+    private GameObject mapGenerator;
+    private Vector2 mousePosition;
+
+    private void Start(){
+        mapGenerator = GameObject.Find("MapGenerator");
+    }
+
+    private void Update(){
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.x = Mathf.Floor(mousePosition.x + 0.5f);
+        mousePosition.y = Mathf.Floor(mousePosition.y + 0.5f);
+        if (Input.GetMouseButtonDown(0)){
+            
+        }
+    }
 }

f1a80ec04a02759379369375298d621c46eea3eb
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2017-12-05 18:08:45 -0500

    Rangemen des sons, suprresion de variable inutilees et suppression de l'animator pour la camera dans le menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour {
-    public Button bouttonJouer;
-    public Button boutonQuitter;
-    public GameObject sceneMapAleatoire;
-
-	public void quitter()
-    {
+public class Menu : MonoBehaviour{
+    public void quitter(){
         Application.Quit();
     }
 
-    public void jouer()
-    {
+
+    public void jouer(){
         SceneManager.LoadScene("MapAleatoire", LoadSceneMode.Single);
     }
 }
